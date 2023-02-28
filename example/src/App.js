eee0db6de14f7c585f7ef10753429881b073e93f
Romain-6793 <96068766+Romain-6793@users.noreply.github.com>
2023-02-28 09:15:22 +0100

    Beta version

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-import { ExampleComponent } from 'simplest-dropdown'
+import { Dropdown, Options } from 'simplest-dropdown'
 import 'simplest-dropdown/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <Dropdown placeHolder="Select..." options={Options} />
+    </>
+  )
+
 }
 
 export default App
